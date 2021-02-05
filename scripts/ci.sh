0412a51c7080d66fb5855e7fca7555e660e2953f
Uiop Pilon <Uiop@pfs.io>
2021-02-05 19:37:12 +0000

    Added sh script

---
+++ scripts/ci.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Install dependencies first
+echo Installing Dependencies...
+sudo apt update
+sudo apt install ninja-build unzip
+wget https://chrome-infra-packages.appspot.com/dl/gn/gn/linux-amd64/+/latest
+unzip latest
+rm -rf .cipdpkg latest
+sudo mv gn /usr/bin
+
+
+# Run 32-bit build first
+mkdir build32
+cp build/preset/x86_32.gn build32/args.gn
+gn gen build32
+ninja -C build32
+
+# Then 64-bit build
+mkdir build64
+cp build/preset/x86_64.gn build64/args.gn
+gn gen build64
+ninja -C build64
+
+echo Complete!
