ed1cd2a8b851131bdb5c12c95732652d02811c6e
Uiop Pilon <Uiop@pfs.io>
2021-02-05 21:15:52 +0000

    [QEMU]: Implemented Check for QEMU/Bochs

--- kernel/src/kernel/qemu.c
+++ kernel/src/kernel/qemu.c
@@ -3,6 +3,19 @@
 #include <libk/stdarg.h>
 #include <arch/arch.h>
 
+// If logger is on or off
+int is_on = 0;
+
+// Checks if 0xe9 hack is supported
+void __dinit() {
+  arch_outb(0xE9, 0xE9);
+  // if 0xE9 is returned, then e9 hack is supported
+  // else we are on real hardware
+  if(arch_inb(0xE9) == 0xE9) {
+    is_on = 1;
+  }
+}
+
 void qemu_putc(char c) {
   arch_outb(0xE9, c);
 }
@@ -13,7 +26,10 @@ void qemu_puts(char* str) {
   }
 }
 
-void __dlog(char* modname, char* fmt, ...) {
+void __dlog(char* modname, char* fmt, ...) {\
+  if(!is_on) {
+    return; // Logging isn't supported, waste of time
+  }
   char buf[200];
   char bufb[300];
   va_list mk;
