c1b23d8a1e508543ba06a37584fde8a20e199de1
Uiop Pilon <Uiop@pfs.io>
2021-02-06 18:44:04 +0000

    [TTY]: (x86-64) Added Console Support

--- arch/x86-64/src/boot.c
+++ arch/x86-64/src/boot.c
@@ -12,18 +12,15 @@ handover_t trt;
 static void init_handover() {
 	trt.arch_name = "x86-64";
 	trt.arch_type = 2;
-	trt.framebuffer = (void*)0xB8000;
-	trt.height = 25;
-	trt.width = 80;
-	trt.pitch = 0;
+	trt.framebuffer = (void*)fb;
+	trt.height = bootboot.fb_height;
+	trt.width = bootboot.fb_width;
+	trt.pitch = 32;
 }
 
 void _start() {
   DINIT();
   DLOG("Just Booted!");
 	init_handover();
-  for(;;) {
-    __asm volatile ("hlt");
-  }
-//	kmain(&trt);
+  kmain(&trt);
 }
